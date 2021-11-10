d7e47e4239d76eb99be33ab016cba64cb8274e35
rutujab006 <rb264@njit.edu>
2021-11-09 22:20:03 -0500

    Fixed an error

--- calc/subtraction.py
+++ calc/subtraction.py
@@ -1,9 +1,10 @@
 """This is subtraction operation object"""
 
-from calc.calculations.calculation import Calculation
+from calc.calculation import Calculation
 
 class Subtraction(Calculation):
     """This is subtraction class"""
     def get_result(self):
         """This is subtraction class"""
         return self.value_a - self.value_b
+    
